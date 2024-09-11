5f2994c24be4a2f36026537c381b6562abde4559
Gabriel <g4briel.ian19@gmail.com>
2024-09-11 10:51:26 -0300

    Placing Pieces on the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,8 +2,8 @@ package boardgame;
 
 public class Piece {
 
-    private Position position;
-    protected Board board;
+    protected Position position;
+    private Board board;
 
     public Piece(Board board) {
         this.board = board;
